9e2fa93ae50ca008d6dcac74572ad412426ed85e
rvosa <rutgeraldo@gmail.com>
2011-04-03 22:12:14 +0100

    Adding PhyloWS controller for matrices

---
+++ lib/Bio/Phylo/PhyloBank/Controller/PhyloWS/Matrix.pm
@@ -0,0 +1,44 @@
+package Bio::Phylo::PhyloBank::Controller::PhyloWS::Matrix;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Bio::Phylo::PhyloBank::Controller::PhyloWS::Matrix - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Bio::Phylo::PhyloBank::Controller::PhyloWS::Matrix in PhyloWS::Matrix.');
+}
+
+
+=head1 AUTHOR
+
+Rutger Vos
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;

c14038cebcb91190724a56a4b61c16bae8d4e743
rvosa <rutgeraldo@gmail.com>
2011-04-04 01:45:23 +0100

    Adding Study model

---
+++ lib/Bio/Phylo/PhyloBank/Model/Study.pm
@@ -0,0 +1,27 @@
+package Bio::Phylo::PhyloBank::Model::Study;
+use base 'Catalyst::Model::Adaptor';
+
+__PACKAGE__->config( class => 'Bio::Phylo::Project' );
+
+sub mangle_arguments {}
+
+=head1 NAME
+
+Bio::Phylo::PhyloBank::Model::Study - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Rutger Vos
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
